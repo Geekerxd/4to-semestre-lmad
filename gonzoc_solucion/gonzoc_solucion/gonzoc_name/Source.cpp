e16318f2bb571cc62766ae81b77446b12fb9f4f5
Gonza_Anorak <geeker00513@gmail.com>
2019-09-03 07:28:54 -0500

    no message

--- gonzoc_solucion/gonzoc_solucion/gonzoc_name/Source.cpp
+++ gonzoc_solucion/gonzoc_solucion/gonzoc_name/Source.cpp
@@ -1,156 +1,58 @@
-//#include <iostream>
-////#include <math.h>
-//using namespace std;
-//
-//char infija[12] = {"A-B/(C*D^E)"};
-//char postfija[12];
-//char pila[10];
-//
-//void push(int i);
-//void pull(int i);
-//
-//int c = 0;
-//int D = 0;
-//
-////enum operadores {
-////	mas,
-////	menos= 1;
-////	
-////	^,
-////};
-//
-//
-//void push(int i) {   //mete a la pila
-//	pila[c] = infija[i];
-//	
-//}
-//
-//void pull(int i) {  //mete a la posfija  ???
-//
-//}
-//
-//void main() {
-//	//system("color 1A");
-//
-//	for (int i = 0; i<12; i++) {
-//		switch (infija[i]) {
-//		case '(':
-//			push(i);
-//			c++;
-//
-//			break;
-//		case ')':
-//
-//			//aquo una funcion que siga hasta )
-//
-//			break;
-//		case '+':
-//			if (c == 0 || pila[c] == 40 || pila[c] == 42 || pila[c] == 47 || pila[c] == 40) {
-//				push(i);
-//			}
-//			else {
-//				if (pila[c] == 45 || pila[c] == 43) {
-//					postfija[c] = pila[c];
-//					push(i);
-//				}
-//			}
-//			break;
-//		case '-':
-//			if (c == 0 || pila[c] == 40) {
-//				push(i);
-//				c++;
-//			}
-//			else{
-//				if (pila[c] == 45 || pila[c] == 43) {
-//					postfija[c] = pila[c];
-//					push(i);
-//				}
-//			}
-//
-//			break;
-//	/*	case :
-//
-//			cout << "h";
-//			system("pause>NUL");
-//			break;*/
-//		case '/':
-//			if (c == 0 || pila[c] == 40 || pila[c] == 45 || pila[c] == 43) { //si es mayor igual
-//				push(i);
-//				c++;
-//			}
-//			else {
-//				if (pila[c] == 45 || pila[c] == 43) {
-//					
-//					push(i);
-//				}
-//				else if(pila[c] == 42 || pila[c] == 47|| pila[c] == 94) { //si es menor igual
-//					postfija[c] = pila[c];
-//					push(i);
-//				}
-//			}
-//			break;
-//		case '*':
-//
-//			break;
-//		case '^':
-//
-//			break;
-//		default:
-//			
-//
-//			if (infija[i] >= 48 && infija[i] <= 57 || infija[i] >= 65 && infija[i] <= 90 || infija[i] >= 97 && infija[i] <= 122) {
-//			
-//				postfija[D] = infija[i];
-//				D++;
-//
-//			}
-//          break;
-//
-//			
-//		}
-//
-//
-//		
-//	}
-//	
-//
-//	//system("pause>NUL");
-//}
-
-
 #include <iostream>
-//#include <math.h>
+#include <math.h>
+#include <cstring>
+#include <string>
 using namespace std;
 
-char infija[12] = { "A-B/(C*D^E)" };
-char postfija[12];
-char pila[10];
-
-void push(int);
-void pull(int);
+char infija[12] = {"A-B/(C*D^E)"};
+string postfija;
+string pila;
+char gg[];
+void push(int i, string exp);
+void pull(int c);
 void imprimir();
-
 int c = 0;
 int d = 0;
 
-void push(int i) {
-	pila[c] = infija[i];
-	c++;
+int tamano;
+
+//protocolos
+void Postfija(string exp);
+
+int main() {
+
+	
+	string expresion;
+	cout << "\nIngresa algo:  ";
+	getline(cin, expresion);
+	cout << "\nUsted ingreso: " << expresion << " de tamaño: " << expresion.length()<<endl; 
+	tamano = expresion.length();
+
+
+	Postfija(expresion);
+
+	system("pause>NUL");
+	return 0;
 }
+void Postfija(string exp) {
+	postfija.assign(exp, d,d+1);
+	cout << postfija;
+	d++;
+	postfija.assign(exp, d, d + 1);
+	cout << postfija;
+	d++;
+	postfija.assign(exp, d, d + 1);
+	cout << postfija;
+	d++;
+	postfija.assign(exp, d, d + 1);
+	cout << postfija;
+	d++;
 
-void pull(int c) {
-	for (int cc = c - 1; cc >= 0; cc--) {
-		postfija[d] = pila[cc];
-		d++;
-	}
-}
-
-void main() {
-
-	for (int i = 0; i < 12; i++) {
-		switch (infija[i]) {
+	system("pause>nul");
+	for (int i = 0; i < tamano; i++) {
+		switch (exp[i]) {
 		case '(':
-			push(i);
+			push(i, exp);
 			break;
 
 		case ')':
@@ -159,62 +61,62 @@ void main() {
 
 		case '+':
 			if (c == 0 || pila[c - 1] == 40) {
-				push(i);
+				push(i,exp);
 			}
 			else {
 				if (pila[c - 1] == 45 || pila[c - 1] == 43 || pila[c - 1] == 42 || pila[c - 1] == 47 || pila[c - 1] == 40 || pila[c - 1] == 94) {
 					postfija[d] = pila[c - 1];
 					d++;
-					push(i);
+					push(i, exp);
 				}
 			}
 			break;
 
 		case '-':
 			if (c == 0 || pila[c - 1] == 40) {
-				push(i);
+				push(i, exp);
 			}
 			else {
 				if (pila[c - 1] == 45 || pila[c - 1] == 43 || pila[c - 1] == 42 || pila[c - 1] == 47 || pila[c - 1] == 94) {
 					postfija[d] = pila[c - 1];
 					d++;
-					push(i);
+					push(i, exp);
 				}
 			}
 			break;
 
 		case '/':
 			if (c == 0 || pila[c - 1] == 40 || pila[c - 1] == 45 || pila[c - 1] == 43) {
-				push(i);
+				push(i, exp);
 			}
 			else if (pila[c - 1] == 42 || pila[c - 1] == 47 || pila[c - 1] == 94) {
 
 				postfija[d] = pila[c - 1];
 				d++;
-				push(i);
+				push(i, exp);
 			}
 			break;
 
 		case '*':
 			if (c == 0 || pila[c - 1] == 40 || pila[c - 1] == 45 || pila[c - 1] == 43) {
-				push(i);
+				push(i, exp);
 			}
 			else if (pila[c - 1] == 42 || pila[c - 1] == 47 || pila[c - 1] == 94) {
 
 				postfija[d] = pila[c - 1];
 				d++;
-				push(i);
+				push(i, exp);
 			}
 			break;
 
 		case '^':
 			if (c == 0 || pila[c - 1] == 40 || pila[c - 1] == 45 || pila[c - 1] == 43 || pila[c - 1] == 42 || pila[c - 1] == 47) {
-				push(i);
+				push(i, exp);
 			}
 			else if (pila[c - 1] == 94) {
 				postfija[d] = pila[c - 1];
 				d++;
-				push(i);
+				push(i, exp);
 			}
 			break;
 
@@ -229,11 +131,23 @@ void main() {
 
 	imprimir();
 
-	//_getch();
-	//system("pause");
+
+}
+
+void push(int i, string exp) {
+	pila[c] = exp[i];
+	c++;
+}
+
+void pull(int c) {
+	for (int cc = c - 1; cc >= 0; cc--) {
+		postfija[d] = pila[cc];
+		d++;
+	}
 }
 
 void imprimir() {
+	cout << endl;
 	for (int i = 0; i < 12; i++) {
 		if (postfija[i] != 40) {
 			cout << postfija[i];
