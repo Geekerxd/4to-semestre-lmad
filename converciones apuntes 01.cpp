c47ac6fd886eb71fabeb1ed99018bc120be929d5
Gonzalo Aguilas Galeana <gag_g249A@hotmail.com>
2019-10-03 09:02:45 -0500

    no message

--- converciones apuntes 01.cpp
+++ converciones apuntes 01.cpp
@@ -7,7 +7,7 @@
  Num1 = strtod(e1,0);                         //char to float                               
  _itoa (result[0 ][0  ] ,R_11  ,10);          // integer to char
  
- //integer to float  
+ //....................                       //integer to float  
  int i;
  float j;
  j=(float)i;
@@ -15,27 +15,28 @@
  
  
  a = stoi(b);                                    //string to int
- 
+ strcpy_s( name02, (*name01).c_str() );          //string to char
  
  /*              Uso de chars                */
  strcmp ( const char * str1, const char * str2 );
                                                  //compara dos chars
  strcpy ( char * destination, const char * source );
+                                                    //Char to Char
                                                     //copia el contenido de un char a otro (completo)
+													
+													
+													
+													
  char * strcat ( char * destination, const char * source );
                                                       //concatena un char al contenido de otro
  
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
   snprintf(R_11 , sizeof R_11 , "%f",result[0 ][0  ]);
+  
+  
+  ((*name01).length());                              //mide lo largo de un String
+  
+  
+  
   
    Num1 = strtod(e1,0); 
    matrixA[0 ][0  ] = strtod(A_11,0   );
